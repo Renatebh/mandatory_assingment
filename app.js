9acfdf85cb8cc698c64cac50a359286adc3831f6
Renatebh <renate.hem@gmail.com>
2022-11-09 15:15:36 +0100

    message

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
-
+const moment = require("moment");
 const bodyParser = require("body-parser");
 // const morgan = require("morgan");
 // const fs = require("fs");
@@ -33,7 +33,7 @@ let items = [];
 
 app.post("/item", (req, res) => {
   const item = req.body;
-
+  console.log(item);
   items.push(item);
   console.log(items);
   res.send(`Item whith name ${item.name} added to the items array`);
@@ -43,26 +43,43 @@ app.get("/items", (req, res) => {
   res.send(items);
 });
 
-const CREATE_USERS = db.run(
-  "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, category TEXT, price INTEGER, cardnumber INTEGER)"
-);
+// db.run(
+//   "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, category TEXT, price INTEGER, cardnumber INTEGER, store TEXT, location TEXT, date TEXT  )"
+// );
 
 app.post("/card", (req, res) => {
   const card = req.body.cardnumber;
+  const store = req.body.store;
+  const location = req.body.location;
+  let date = moment().format("DD.MM.YY");
 
   items.forEach((item) => {
     item.cardnumber = card;
   });
-  console.log("Items", items);
 
-  db.run(CREATE_USERS, () => {
-    res.send("Table created");
+  items.forEach((item) => {
+    item.store = store;
   });
 
-  db.serialize(() => {
-    db.run(
-      db.run(
-        "INSERT INTO users (name, category, price, cardnumber) VALUES ('" +
+  items.forEach((item) => {
+    item.location = location;
+  });
+
+  console.log("Items", items);
+  res.send(`Card with cardnumber ${card} added`);
+
+  items.map((item) => {
+    let name = item.name;
+    let category = item.category;
+    let price = item.price;
+    let cardnumber = item.cardnumber;
+    // let location = item.location;
+    // let store = item.store;
+    // let date = item.newDate;
+
+    db.serialize(() => {
+      db.each(
+        "INSERT INTO users (name, category, price, cardnumber, store, location, date) VALUES ('" +
           name +
           "',  '" +
           category +
@@ -70,15 +87,82 @@ app.post("/card", (req, res) => {
           price +
           "',  '" +
           cardnumber +
+          "', '" +
+          location +
+          "', '" +
+          store +
+          "', '" +
+          date +
           "');"
       ),
-      (err) => {
+        (err) => {
+          if (err) {
+            return console.log(err.message);
+          }
+          console.log("New user has been added");
+          res.send("New user added");
+          return res.json();
+        };
+    });
+  });
+});
+
+app.get("/card/:card_number", (req, res) => {
+  let data = [];
+  db.serialize(() => {
+    db.each(
+      "SELECT * FROM users WHERE cardnumber = ?;",
+      [req.params.card_number],
+      (err, row) => {
         if (err) {
-          return console.log(err.message);
+          res.send("Error occur while updating");
+          return console.error(err.message);
         }
-        console.log("New user has been added");
-        res.send("New user added");
-        return res.json();
+        data.push(row);
+      },
+      () => {
+        res.send(data);
+      }
+    );
+  });
+});
+
+// GET DATA BY DATE DD.MM.YY
+app.get("/day/:date", (req, res) => {
+  let data = [];
+  db.serialize(() => {
+    db.each(
+      "SELECT * FROM users WHERE date = ?;",
+      [req.params.date],
+      (err, row) => {
+        if (err) {
+          res.send("Error occur while updating");
+          return console.error(err.message);
+        }
+        data.push(row);
+      },
+      () => {
+        res.send(data);
+      }
+    );
+  });
+});
+
+app.get("/month/:month_number/:year_number", (req, res) => {
+  let data = [];
+  db.serialize(() => {
+    db.each(
+      "SELECT * FROM users WHERE date = ?;",
+      [req.params.mont.month_number.year_number],
+      (err, row) => {
+        if (err) {
+          res.send("Error occur while updating");
+          return console.error(err.message);
+        }
+        data.push(row);
+      },
+      () => {
+        res.send(data);
       }
     );
   });
