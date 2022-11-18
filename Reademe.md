ca336a970650d22adb070f81f460688cb784d117
Renatebh <renate.hem@gmail.com>
2022-11-18 08:58:03 +0100

    code is done

--- Reademe.md
+++ Reademe.md
@@ -20,12 +20,10 @@ Node moduls must be reinstalled, and is important to make a node install in the 
 
 You can use [Postman](https://www.postman.com/), or any other program, to send data from browswer to api. Postman is the "frontend" in this project and it's easier if you've it installed. But you can also use the browser as well.
 
-```bash
-npm install
-```
+To start this application run these commands in your terminal:
 
 ```bash
-node app.js
+npm install
 ```
 
 ```bash
@@ -38,9 +36,23 @@ npm start
 | Post items                              | Post         | http://localhost:8080/item                    | {"name": "item-name", "category":"categorys", "price":"price"} |
 | Get items from array                    | Get          | http://localhost:8080/items                   |                                                                | An array of items                                                                                                          |
 | Post cardnumber, store, location        | Post         | http://localhost:8080/card                    | {"cardnumber":"1234", "store":"Kiwi","location":"Sandefjord"}  | {"name": "item-name", "category":"categorys", "price":"price","cardnumber":"1234", "store":"Kiwi","location":"Sandefjord"} |
-| Get all items registred to a cardnumber | Get          | http://localhost:8080/card/:card_number       | b                                                              | JSON data, a list of items registred the cardnumber                                                                        |
-| Get data when search on store           | Get          | http://localhost:8080/store/:store_name       | b                                                              | JSON data with name, categories, price,store,location                                                                      |
-| Get data when search on location        | Get          | http://localhost:8080/location/:location_name | b                                                              | JSON data with name, categories, price,store,location                                                                      |
-| Get data when search on month and year  | Get          | http://localhost:8080/month/month_month       | b                                                              | JSON data with name, categories, price,store,location                                                                      |
-| Get data when search on date            | Get          | http://localhost:8080/day/:date               | b                                                              | JSON data with name, categories, price,store,location                                                                      |
-| Delete all data on with a cardnumber    | Delete       | http://localhost                              |
+| Get all items registred to a cardnumber | Get          | http://localhost:8080/card/:card_number       |                                                                | JSON data, a list of items registred the cardnumber                                                                        |
+| Get data when search on store           | Get          | http://localhost:8080/store/:store_name       |                                                                | JSON data with name, categories, price,store,location                                                                      |
+| Get data when search on location        | Get          | http://localhost:8080/location/:location_name |                                                                | JSON data with name, categories, price,store,location                                                                      |
+| Get data when search on month and year  | Get          | http://localhost:8080/month/month_month       |                                                                | JSON data with name, categories, price,store,location                                                                      |
+| Get data when search on date            | Get          | http://localhost:8080/day/:date               |                                                                | JSON data with name, categories, price,store,location                                                                      |
+| Delete all data on with a cardnumber    | Delete       | http://localhost:8080/card/:card_number       |                                                                | JSON data with name, categories, price,store,location                                                                      |
+
+## Dependencies used in this assingment
+
+- Express:
+  Express is a Node.js web framework that provides a set of features for web and mobile applications.
+- Sqlite 3:
+  I’m using SQLITE 3 to make my database for this project. It’s a database that doesn’t require a seperate server. Verbose is for debbugging. The mode is set to verbose to produce long stack traces. When you throw an error from a callback passed to any of the database functions, node-sqlite3 will append the stack trace information from the original call.
+- Body-parser:
+  It provides four express middleware for parsing JSON, Text, URL-encoded, and raw data over an HTTP request body. I used body-parser for parsing JSON data.
+
+- Moment:
+  Parse, validate, manipulate, and display dates and times in JavaScript. I used it for parse in date in the format "DD.MM.YY".
+
+- Nodemon: For development, nodemon is starting the server when changes are made.
--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 /*
  * Copyright Renate Hem
- *
- * This is the main entrypoint of my application
  */
 
 const sqlite3 = require("sqlite3").verbose();
@@ -9,19 +7,15 @@ const express = require("express");
 const moment = require("moment");
 const bodyParser = require("body-parser");
 
-const http = require("http");
 const hostname = "localhost";
 const port = process.env.PORT || 8080;
 const app = express();
-const server = http.createServer(app);
 
 app.use(bodyParser.json());
-app.use(express.json()); //THIS IS TO ACCEPT JSON FORMAT
+app.use(express.urlencoded({ extended: true }));
 
-// MAKING DB FILE
 const db = new sqlite3.Database("./database/users.db");
 
-// CATEGORYS ARRAY USER MUST CHOSE FROM
 const categorys = [
   "beverage",
   "bakery",
@@ -35,9 +29,15 @@ const categorys = [
 ];
 
 app.get("/", (req, res) => {
+  if (!categorys) {
+    return res.status(400).json({ error: "No data" });
+  }
   res.send(categorys);
 });
 
+/*
+ * POST ITEMS IN THE ITEMS ARRAY, IT RETURNS A CHECK IF YOU HAVE ADDED ITEMS WITH THE RIGHT CATEGORY. IF YOU DONT CHOOSE RIGHT CATEGORY, ITEMS WILL NOT BE PUSHED INTO ITEMS ARRAY.
+ */
 let items = [];
 
 app.post("/item", (req, res) => {
@@ -48,22 +48,25 @@ app.post("/item", (req, res) => {
   if (categorys.includes(category.toLowerCase()) || category === "") {
     items.push(item);
     res.send(`Item whith name ${item.name} added to the items array`);
+  } else if (!categorys) {
+    return res.status(404).json({ error: "No data" });
   } else {
     res.send(`You must chose a category from list or leave it blank`);
     return;
   }
-  console.log(items);
 });
 
-// GET ITEMS FROM ITEMS(array)
+//! Development endpoint, Only for checking if the items exists, returning the array of items.
 app.get("/items", (req, res) => {
   res.send(items);
 });
 
+// ** CREATING TABLE CALLED USERS
 db.run(
-  "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, category TEXT, price INTEGER, cardnumber INTEGER, store TEXT, location TEXT, date TEXT  )"
+  "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, category TEXT, price INTEGER NOT NULL, cardnumber INTEGER NOT NULL, store TEXT NOT NULL, location TEXT NOT NULL, date TEXT NOT NULL  )"
 );
 
+// * THIS ENDPIONT IS ADDING MORE INFORMATION ON THE ITEMS: CARDNUMBER, STORE AND LOCATION. IM ADDING IT TO THE ARRAY BEFORE IT'S PUSHED INTO THE DATABASE. IF ITEMS ARRAY IS EMPTY IT WILL RETURN A MESSAGE THAT IT IS AND IT WILL NOT BE PUSHED, OTHERWISE IT WILL RETURN A MESSAGE THAT ITEMS IS PAID FOR. (201)
 app.post("/card", (req, res) => {
   const card = req.body.cardnumber;
   const store = req.body.store;
@@ -82,12 +85,6 @@ app.post("/card", (req, res) => {
   items.forEach((item) => {
     item.location = location;
   });
-
-  console.log(
-    "Items added to database with card/store/location information",
-    items
-  );
-  res.send(items);
 
   items.map((item) => {
     let name = item.name;
@@ -112,64 +109,90 @@ app.post("/card", (req, res) => {
           "', '" +
           date +
           "');"
-      ),
-        (err) => {
-          if (err) {
-            res.send("Error occur while adding to database");
-            return console.log(err.message);
-          }
-          console.log("New user has been added");
-          res.send("New user added");
-          return res.json();
-        };
+      );
     });
   });
+
+  if (items.length === 0) {
+    return res.status(404).send("Items array is empty, you must add Items");
+  } else {
+    items = [];
+    return res.status(201).send("Items paid! Thank you..");
+  }
 });
 
-// GET DATA (item-name only) BY CARD NUMBER - 4 DIGITS
+/*
+ * -GET DATA FROM TABLE BY CARD NUMBER - 4 DIGITS - RETURNING (name, store, location, cardnumber) DATA FROM A CARDNUMBER, OR A MESSAGE SAYING CARDNUMBER DON'T EXIST.
+ */
 app.get("/card/:card_number", (req, res) => {
   let data = [];
 
   db.serialize(() => {
     db.each(
-      "SELECT name FROM users WHERE cardnumber = ?;",
-      [req.params.card_number],
+      "SELECT name, store, location, cardnumber FROM users WHERE cardnumber = ?;",
+      req.params.card_number,
       (err, row) => {
         if (err) {
-          res.send("Error occur while displaying card details");
-          return console.error(err.message);
+          return res.status(404).json({ error: "Error occur" });
         }
         data.push(row);
       },
+
       () => {
-        res.send(data);
+        let cardData = [];
+
+        data.map((card) => {
+          cardData = card.cardnumber;
+        });
+
+        if (cardData != parseInt(req.params.card_number)) {
+          return res
+            .status(404)
+            .json({ error: "Cardnumber does not exists in database" });
+        }
+
+        return res.send(data);
       }
     );
   });
 });
 
+/*
+ * GET DATA BY STORE NAME, RETURNS AN ARRAY OFF DATA REGISTRED TO THAT STORE. I USED MAP TO COMPARE THE VALUES IN STOREDATA AND STORENAME AND TO CHECK IF STORENAME EXITS, FOR WRITING THE IF STATMENT.
+ */
 app.get("/store/:store_name", (req, res) => {
   let data = [];
+  let storeName = req.params.store_name;
 
   db.serialize(() => {
     db.each(
-      "SELECT * FROM users WHERE store = ?;",
-      [req.params.store_name],
+      "SELECT * FROM users WHERE store LIKE '" + storeName + "%';",
       (err, row) => {
         if (err) {
-          res.send("Error occur while displaying store details");
-          return console.error(err.message);
+          return res.status(404).json({ error: "Error occur" });
         }
         data.push(row);
       },
+
       () => {
-        res.send(data);
+        let storeData = [];
+
+        data.map((store) => {
+          storeData = store.store;
+        });
+
+        if (storeData.includes(storeName)) {
+          return res.send(data);
+        } else
+          return res
+            .status(404)
+            .json({ error: "Store does not exists in database" });
       }
     );
   });
 });
 
-// GET DATA FROM A LOCATION
+// * GET DATA FROM A LOCATION RETURNING AN ARRAY OF ITEMS BOUGHT ON THAT LOCATION
 app.get("/location/:location_name", (req, res) => {
   let data = [];
 
@@ -179,19 +202,32 @@ app.get("/location/:location_name", (req, res) => {
       [req.params.location_name],
       (err, row) => {
         if (err) {
-          res.send("Error occur while displaying store details");
-          return console.error(err.message);
+          return res.status(404).json({ error: "Error occur" });
         }
         data.push(row);
       },
+
       () => {
-        res.send(data);
+        let locationData = [];
+
+        data.map((location) => {
+          locationData = location.location;
+        });
+
+        if (locationData === req.params.location_name) {
+          return res.send(data);
+        }
+        return res
+          .status(404)
+          .json({ error: "Location does not exists in database" });
       }
     );
   });
 });
 
-// GET DATA BY DATE DD.MM.YY
+/*
+ * GET DATA BY DATE DD.MM.YY - RETURNING ALL ITEMS BOUGHT ON THAT DAY - RETURNING AN MESSAGE IF DATE DONT'T EXIST
+ */
 app.get("/day/:date", (req, res) => {
   let data = [];
 
@@ -201,19 +237,29 @@ app.get("/day/:date", (req, res) => {
       [req.params.date],
       (err, row) => {
         if (err) {
-          res.send("Error occur while updating");
-          return console.error(err.message);
+          return res.status(404).json({ error: "Error occur" });
         }
         data.push(row);
       },
       () => {
-        res.send(data);
+        let dateData = [];
+
+        data.map((date) => {
+          dateData = date.date;
+        });
+
+        if (dateData === req.params.date) {
+          return res.send(data);
+        }
+        return res
+          .status(404)
+          .json({ error: "Date does not exists in database" });
       }
     );
   });
 });
 
-// GET DATA BY MONTH AND YEAR
+// * GET DATA BY MONTH AND YEAR - RETURNING ALL DATA BOUGTH IN ONE MONTH AND YEAR. CHECK IF DATE EXISTS - RETURNS A MESSAGE IF DATE NOT FOUND
 app.get("/month/:month_number/:year_number", (req, res) => {
   let data = [];
   let month = req.params.month_number;
@@ -225,21 +271,28 @@ app.get("/month/:month_number/:year_number", (req, res) => {
 
       (err, row) => {
         if (err) {
-          res.send("Error occur");
-          return console.error(err.message);
+          return res.sendStatus(404).res.send("Error occur");
         }
-
         data.push(row);
-        console.log("Data", data);
       },
       () => {
-        res.send(data);
+        let monthYearData = [];
+        data.map((date) => {
+          monthYearData = date.date;
+        });
+
+        if (monthYearData.includes(month + "." + year)) {
+          res.send(data);
+        }
+        return res
+          .status(404)
+          .json({ error: "Month and year does not exists in database" });
       }
     );
   });
 });
 
-// DELETE DATA FROM A CARDNUMBER
+// * DELETE DATA FROM A CARDNUMBER - RETURNING "DATA DELETED" OR 400 IF IT IS A BAD REQ.
 app.delete("/card/:card_number", (req, res) => {
   db.serialize(() => {
     db.run(
@@ -248,18 +301,16 @@ app.delete("/card/:card_number", (req, res) => {
 
       (err) => {
         if (err) {
-          res.sendStatus(404);
-          return console.error(err.message);
+          return res.sendStatus(400);
         }
-        res.sendStatus(204);
-
-        console.log("Data deleted");
+        res.send("Data deleted");
       }
     );
   });
 });
 
-// CLOSE DATABASE
+// * CLOSE DATABASE
+// ! Not a requirement
 app.get("/close", (req, res) => {
   db.close((err) => {
     if (err) {
@@ -271,19 +322,6 @@ app.get("/close", (req, res) => {
   });
 });
 
-// // DELETE DATABASE
-// app.delete("/delete_database", (req, res) => {
-//   db.run((err) => {
-//     if (err) {
-//       res.send("There is some error in deleting the database");
-//       return console.error(err.message);
-//     }
-//     `DROP TABLE users`;
-//     console.log("Database deleted.");
-//     res.send("Database is successfully deleted");
-//   });
-// });
-
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`server is running at http://${hostname}:${port}`);
 });
